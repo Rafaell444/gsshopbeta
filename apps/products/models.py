532e83465c39843e8ff1812775eeb43194b4ce57
Rafaell444 <raphaeli.mkhitarian.1@btu.edu.ge>
2022-12-28 16:43:41 +0400

    products added\percentage added

--- apps/products/models.py
+++ apps/products/models.py
@@ -7,8 +7,16 @@ class Product(models.Model):
     slug = models.SlugField(unique=True)
     product_description = models.TextField(max_length=512)
     category = models.ForeignKey(SubSubCategory, on_delete=models.DO_NOTHING)
+    price = models.DecimalField(max_digits=7, decimal_places=2, blank=False)
+    sale_percentage = models.DecimalField(max_digits=7, decimal_places=2, blank=True, default=0)
+    sale_price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return self.product_name
 
-
+    def save(self, *args, **kwargs):
+        if self.sale_percentage > 0:
+            self.sale_price = self.price * (self.sale_percentage / 100)
+        else:
+            self.sale_price = None
+        super(Product, self).save(*args, **kwargs)
--- apps/store/views.py
+++ apps/store/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from apps.categories.models import Category, SubCategory, SubSubCategory
+from apps.products.models import Product
 
 
 def store(request):
     main_categories = Category.objects.all()
     sub_categories = SubCategory.objects.all()
     sub_sub_categories = SubSubCategory.objects.all()
+    products = Product.objects.all()
     context = {
         "main_categories": main_categories,
         'sub_categories': sub_categories,
         'sub_sub_categories': sub_sub_categories,
+        'products': products
     }
     return render(request, 'store/main.html', context)
 
